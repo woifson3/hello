82ea79ebcc79c2ab6fe6d8d94a7773ce3e934846
san <343492382@qq.com>
2019-10-09 16:39:29 +0800

    aaaa

---
+++ HelloFriend/src/main/java/com/atguigu/maven/HelloFriend.java
@@ -0,0 +1,17 @@
+package com.atguigu.maven;
+
+import com.atguigu.maven.Hello;
+
+public class HelloFriend {
+	public String sayHelloToFriend(String name) {
+		Hello hello = new Hello();
+		String str = hello.sayHello(name) + " I am " + this.getMyName();
+		System.out.println(str);
+		return str;
+	}
+
+	public String getMyName() {
+		//11111
+		return "John";
+	}
+}
